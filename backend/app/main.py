25257caeea19ad9b933fcbea02b7ac0f12c56b55
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2025-03-09 10:12:41 +0200

    update response structure in prediction endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,10 +4,10 @@ from fastapi import FastAPI
 import mlflow
 import pandas as pd
 
-from app.models import BuildingFeatures
+from app.models import BuildingFeatures, PredictionResponse
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")  # Localhost MLflow server
-model = mlflow.sklearn.load_model(model_uri='runs:/12aff15c5bf844a4baabe74af87f07c4/random_forest_model')
+model = mlflow.sklearn.load_model(model_uri='runs:/70d2099670be42f5b539d7d0ea35fc79/random_forest_model')
 
 app = FastAPI()
 
@@ -27,4 +27,4 @@ async def predict_energy(features: List[BuildingFeatures]):
     predictions = model.predict(input_data)
     
    
-    return {"predictions": predictions}
+    return PredictionResponse(Heating_Load_Y1=predictions[0][0], Cooling_Load_Y2=predictions[0][1])
